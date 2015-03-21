61f87f3ee1ee9f2bc34306102be8669994645af5
G-Ark <ganesh.arkalgud@gmail.com>
2015-03-21 16:16:10 +0530

    Sending and recieving SMS

--- app/src/main/java/com/codingwarriors/specialchat/ReceiveMessage.java
+++ app/src/main/java/com/codingwarriors/specialchat/ReceiveMessage.java
@@ -1,15 +1,41 @@
 package com.codingwarriors.specialchat;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
 
 /**
  * Created by Ujjwal on 21-03-2015.
  */
-public class ReceiveMessage extends Activity {
+public class ReceiveMessage extends BroadcastReceiver {
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.receivemessagelayout);
+    public void onReceive(Context context, Intent intent)
+    {
+        //---get the SMS message passed in---
+        Bundle bundle = intent.getExtras();
+        SmsMessage[] msgs = null;
+        String str = "";
+        if (bundle != null)
+        {
+            //---retrieve the SMS message received---
+            Object[] pdus = (Object[]) bundle.get("pdus");
+            msgs = new SmsMessage[pdus.length];
+            for (int i=0; i<msgs.length; i++){
+                msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
+                str += "SMS from " + msgs[i].getOriginatingAddress();
+                str += " :";
+                str += msgs[i].getMessageBody();
+                str += "\n";
+            }
+            //---display the new SMS message---
+            Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
+        }
     }
 }
+
